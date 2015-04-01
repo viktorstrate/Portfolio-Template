9b82fd8bd6722292b0834996ba6703c9eb564534
Viktor Hundahl Strate <viktorstrate@gmail.com>
2015-04-01 16:49:32 +0000

    Finished the header

--- index.php
+++ index.php
@@ -9,8 +9,23 @@
         <link rel="shortcut icon" href="http://cmmedia.a.ssl.fastly.net/bca287819ca0ab6dfc26/25066709/16x16-c.png">
     </head>
     <body>
+    
+        <br>
+        
         <header>
+        
+        <div id="header-content">
             <h1 class="logo">Thumbs</h1>
+            
+            <nav>
+                <ul>
+                    <li>Work</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </div>
+            
         </header>
         
         <div class="container">
@@ -18,39 +33,51 @@
             
                 <div class="imageitem">
                     <img src="https://cmmedia.a.ssl.fastly.net/4c4764438f9ed9a4a5c9/25875982/382x213/crop:58-41_240x140.gif" />
+                    <p>Category Name</p>
                 </div>
                 <div class="imageitem">
                     <img src="https://cmmedia.a.ssl.fastly.net/4c4764438f9ed9a4a5c9/25875982/382x213/crop:58-41_240x140.gif" />
+                    <p>Category Name</p>
                 </div>
                 <div class="imageitem">
                     <img src="https://cmmedia.a.ssl.fastly.net/4c4764438f9ed9a4a5c9/25875982/382x213/crop:58-41_240x140.gif" />
+                    <p>Category Name</p>
                 </div>
                 <div class="imageitem">
                     <img src="https://cmmedia.a.ssl.fastly.net/4c4764438f9ed9a4a5c9/25875982/382x213/crop:58-41_240x140.gif" />
+                    <p>Category Name</p>
                 </div>
                 <div class="imageitem">
                     <img src="https://cmmedia.a.ssl.fastly.net/4c4764438f9ed9a4a5c9/25875982/382x213/crop:58-41_240x140.gif" />
+                    <p>Category Name</p>
                 </div>
                 <div class="imageitem">
                     <img src="https://cmmedia.a.ssl.fastly.net/4c4764438f9ed9a4a5c9/25875982/382x213/crop:58-41_240x140.gif" />
+                    <p>Category Name</p>
                 </div>
                 <div class="imageitem">
                     <img src="https://cmmedia.a.ssl.fastly.net/4c4764438f9ed9a4a5c9/25875982/382x213/crop:58-41_240x140.gif" />
+                    <p>Category Name</p>
                 </div>
                 <div class="imageitem">
                     <img src="https://cmmedia.a.ssl.fastly.net/4c4764438f9ed9a4a5c9/25875982/382x213/crop:58-41_240x140.gif" />
+                    <p>Category Name</p>
                 </div>
                 <div class="imageitem">
                     <img src="https://cmmedia.a.ssl.fastly.net/4c4764438f9ed9a4a5c9/25875982/382x213/crop:58-41_240x140.gif" />
+                    <p>Category Name</p>
                 </div>
                 <div class="imageitem">
                     <img src="https://cmmedia.a.ssl.fastly.net/4c4764438f9ed9a4a5c9/25875982/382x213/crop:58-41_240x140.gif" />
+                    <p>Category Name</p>
                 </div>
                 <div class="imageitem">
                     <img src="https://cmmedia.a.ssl.fastly.net/4c4764438f9ed9a4a5c9/25875982/382x213/crop:58-41_240x140.gif" />
+                    <p>Category Name</p>
                 </div>
                 <div class="imageitem">
                     <img src="https://cmmedia.a.ssl.fastly.net/4c4764438f9ed9a4a5c9/25875982/382x213/crop:58-41_240x140.gif" />
+                    <p>Category Name</p>
                 </div>
                 </div>
             </div>
@@ -60,7 +87,7 @@
                     Open-Source Portfolio Template
                 </div>
                 <div id="footerCopyright">
-                    Copyright (C) 2015
+                    Copyright (C) 2015 Jonathan and Viktor
                 </div>
             </footer>
   </body>
