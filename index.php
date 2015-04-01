61289f9fbc7f08f3f92328d4aa2476244989ea2e
Viktor Hundahl Strate <viktorstrate@gmail.com>
2015-04-01 14:54:33 +0000

    Right before changing the divs to img tags

--- index.php
+++ index.php
@@ -13,22 +13,24 @@
         </header>
         
         <div class="container">
-            <div class="content">
-                <div class="imageitem" style="background-image: url('https://cmmedia.a.ssl.fastly.net/4c4764438f9ed9a4a5c9/25875982/382x213/crop:58-41_240x140.gif');"></div>
-                <div class="imageitem" style="background-image: url('https://cmmedia.a.ssl.fastly.net/4970a048716e5793b01f/24890240/438x169/crop:99-29_240x140/quality(lossless).webp');"></div>
-                <div class="imageitem" style="background-image: url('https://cmmedia.a.ssl.fastly.net/e814e8b45b19a1f67464/24890267/323x196/crop:37-5_240x140/quality(lossless).webp');"></div>
-                
-                <div class="imageitem" style="background-image: url('https://cmmedia.a.ssl.fastly.net/7d69f0340ceab6b40476/24829206/240x140-c.gif');"></div>
-                <div class="imageitem" style="background-image: url('https://cmmedia.a.ssl.fastly.net/ac49955c4d2d9bcee753/24829171/240x140-c/resample(lanczos).jpeg');"></div>
-                <div class="imageitem" style="background-image: url('https://cmmedia.a.ssl.fastly.net/6deb3ecd36baf17f4be5/24890284/294x165/crop:54-9_240x140.jpeg');"></div>
-                
-                
-                <div class="imageitem" style="background-image: url('https://cmmedia.a.ssl.fastly.net/63426093cbbf7c090764/24890286/240x140-c/resample(lanczos).jpeg');"></div>
-                <div class="imageitem" style="background-image: url('https://cmmedia.a.ssl.fastly.net/aa866c8e9b963a14ce14/24890287/357x144/crop:69-0_240x140/quality(lossless).webp');"></div>
-                <div class="imageitem" style="background-image: url('https://cmmedia.a.ssl.fastly.net/186055cec7c530517c2b/24890293/306x270/crop:32-63_240x140/quality(lossless).webp');"></div>
-                <div class="imageitem" style="background-image: url('https://cmmedia.a.ssl.fastly.net/04af5d3e293178e85897/24829024/240x140-c.webp');"></div>
-                <div class="imageitem" style="background-image: url('https://cmmedia.a.ssl.fastly.net/56350befedfcce4ea140/24829017/240x140-c/resample(lanczos).jpeg');"></div>
-                <div class="imageitem" style="background-image: url('https://cmmedia.a.ssl.fastly.net/80c72fae965109986575/24890296/240x140-c.webp')"></div>
+            <div class="sub-container">
+                <div class="content">
+                    <div class="imageitem" style="background-image: url('https://cmmedia.a.ssl.fastly.net/4c4764438f9ed9a4a5c9/25875982/382x213/crop:58-41_240x140.gif');"></div>
+                    <div class="imageitem" style="background-image: url('https://cmmedia.a.ssl.fastly.net/4970a048716e5793b01f/24890240/438x169/crop:99-29_240x140/quality(lossless).webp');"></div>
+                    <div class="imageitem" style="background-image: url('https://cmmedia.a.ssl.fastly.net/e814e8b45b19a1f67464/24890267/323x196/crop:37-5_240x140/quality(lossless).webp');"></div>
+                    
+                    <div class="imageitem" style="background-image: url('https://cmmedia.a.ssl.fastly.net/7d69f0340ceab6b40476/24829206/240x140-c.gif');"></div>
+                    <div class="imageitem" style="background-image: url('https://cmmedia.a.ssl.fastly.net/ac49955c4d2d9bcee753/24829171/240x140-c/resample(lanczos).jpeg');"></div>
+                    <div class="imageitem" style="background-image: url('https://cmmedia.a.ssl.fastly.net/6deb3ecd36baf17f4be5/24890284/294x165/crop:54-9_240x140.jpeg');"></div>
+                    
+                    
+                    <div class="imageitem" style="background-image: url('https://cmmedia.a.ssl.fastly.net/63426093cbbf7c090764/24890286/240x140-c/resample(lanczos).jpeg');"></div>
+                    <div class="imageitem" style="background-image: url('https://cmmedia.a.ssl.fastly.net/aa866c8e9b963a14ce14/24890287/357x144/crop:69-0_240x140/quality(lossless).webp');"></div>
+                    <div class="imageitem" style="background-image: url('https://cmmedia.a.ssl.fastly.net/186055cec7c530517c2b/24890293/306x270/crop:32-63_240x140/quality(lossless).webp');"></div>
+                    <div class="imageitem" style="background-image: url('https://cmmedia.a.ssl.fastly.net/04af5d3e293178e85897/24829024/240x140-c.webp');"></div>
+                    <div class="imageitem" style="background-image: url('https://cmmedia.a.ssl.fastly.net/56350befedfcce4ea140/24829017/240x140-c/resample(lanczos).jpeg');"></div>
+                    <div class="imageitem" style="background-image: url('https://cmmedia.a.ssl.fastly.net/80c72fae965109986575/24890296/240x140-c.webp')"></div>
+                </div>
             </div>
         </div>
     </body>
